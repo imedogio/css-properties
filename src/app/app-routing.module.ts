0926f75c383f8db4f02b7e643855e60c3d28d9d8
giorgi <giorgierqomaishvili61@gmail.com>
2021-11-22 02:55:53 +0400

    center

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FlexComponent } from './flex/flex.component';
+import { MarginComponent } from './margin/margin.component';
+import { PositionComponent } from './position/position.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'flex', component: FlexComponent},
+  {path: 'position', component: PositionComponent},
+  {path: 'margin', component: MarginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PositionComponent } from './position/position.component';
+import { FlexComponent } from './flex/flex.component';
+import { MarginComponent } from './margin/margin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PositionComponent,
+    FlexComponent,
+    MarginComponent
   ],
   imports: [
     BrowserModule,
